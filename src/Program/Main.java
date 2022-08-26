eebe6e289d1ba8b5a70733b3eaf6d2a4bd3643ab
Ivan Karpinskyi <ivankarpinskij.ua@gmail.com>
2022-08-26 09:10:39 +0300

    Initial commit

---
+++ src/Program/Main.java
@@ -0,0 +1,14 @@
+package Program;
+
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[]args){
+
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+
+        ScreenClass screen = ScreenClass.getScreenInstance(arrayList);
+
+    }
+}
---
+++ src/Program/ScreenClass.java
@@ -0,0 +1,109 @@
+package Program;
+
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class ScreenClass {
+    private   JLabel labelArray;
+    private static  ScreenClass screenClass;
+    private static int count;
+    public JLabel getTextField(){
+        return this.labelArray;
+    }
+    private ScreenClass(ArrayList<Integer> arrayList){
+
+        JFrame frame;
+        JTextField textToAdd;
+        JButton  buttonAddElement;
+        JButton  buttonDeleteElement;
+        JButton  buttonSortAscending;
+        JButton  buttonSortDescending;
+
+        labelArray = new JLabel("EMPTY!!!");
+        labelArray.setHorizontalAlignment(JLabel.CENTER);
+        labelArray.setPreferredSize(new Dimension(460,30));
+        labelArray.setFont(new Font(null,Font.BOLD,20));
+
+        textToAdd = new JTextField();
+        textToAdd.setPreferredSize(new Dimension(80,30));
+        textToAdd.setFont(new Font(null,Font.BOLD,20));
+        textToAdd.setHorizontalAlignment(SwingConstants.CENTER);
+        buttonAddElement = new JButton("Add Element");
+        buttonAddElement.addActionListener(e->{
+            if(textToAdd.getText()!=null && !(textToAdd.getText().isEmpty())){
+                arrayList.add(Integer.valueOf(textToAdd.getText()));
+                labelArray.setText(arrayList.toString());
+            }
+        });
+        buttonAddElement.setPreferredSize(new Dimension(150,30));
+
+        buttonDeleteElement = new JButton("Delete ALL");
+        buttonDeleteElement.setPreferredSize(new Dimension(150,30));
+        buttonDeleteElement.addActionListener(e->{
+            arrayList.clear();
+            labelArray.setText(arrayList.toString());
+        });
+
+        JPanel panelEditingArray = new JPanel();
+        panelEditingArray.setLayout(new FlowLayout());
+        panelEditingArray.setSize(500,150);
+        panelEditingArray.setBorder(BorderFactory.createEmptyBorder(25,10,25,20));
+        panelEditingArray.setBounds(0,0,500,150);
+        panelEditingArray.add(labelArray);
+        panelEditingArray.add(textToAdd);
+        panelEditingArray.add(buttonAddElement);
+        panelEditingArray.add(buttonDeleteElement);
+
+
+
+
+
+        buttonSortAscending = new JButton("Sort Ascending");
+        buttonSortAscending.setSize(new Dimension(200,150));
+        buttonSortAscending.addActionListener(e -> {
+
+            SortingClass.variantTask(100,100);
+            SortingClass.measuringTime();
+            SortingClass.sortingArrayByStepsTest(arrayList);
+            SortingClass.sortAscending(arrayList);
+            labelArray.setText(arrayList.toString());
+
+        });
+
+        buttonSortDescending = new JButton("Sort Descending");
+        buttonSortDescending.setSize(new Dimension(200,150));
+        buttonSortDescending.addActionListener(e -> {
+
+            SortingClass.sortDescending(arrayList);
+            labelArray.setText(arrayList.toString());
+        });
+
+        JPanel sortPanel = new JPanel();
+        sortPanel.setLayout(new FlowLayout());
+        sortPanel.setSize(500,150);
+        sortPanel.setBorder(BorderFactory.createEmptyBorder(0,10,25,20));
+        sortPanel.setBounds(0,150,500,150);
+        sortPanel.add(buttonSortAscending);
+        sortPanel.add(buttonSortDescending);
+
+
+        frame = new JFrame("SortingBuble");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setSize(new Dimension(500,300));
+        frame.add(panelEditingArray);
+        frame.add(sortPanel);
+
+        frame.setVisible(true);
+    }
+    public static ScreenClass getScreenInstance(ArrayList<Integer> arrayList){
+        if(screenClass==null){
+            screenClass = new ScreenClass(arrayList);
+        }
+        return screenClass;
+    }
+
+}
