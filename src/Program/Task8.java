5cfa247f1054a63e61cbe704cf08c40d1242dfc0
Ivan Karpinskyi <ivankarpinskij.ua@gmail.com>
2022-10-03 18:13:09 +0300

    1

--- src/Program/Task8.java
+++ src/Program/Task8.java
@@ -4,44 +4,95 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
-public class Task8 implements Task{
+public class Task8 implements Task {
+
+    private static class Student implements Comparable {
+        String name;
+        int averageMark;
+
+        Student(String name, int averageMark) {
+            this.name = name;
+            this.averageMark = averageMark;
+        }
+
+        @Override
+        public String toString() {
+            return "\nStudent{" +
+                    "name='" + name + '\'' +
+                    ", averageMark=" + averageMark +
+                    '}';
+        }
+
+        @Override
+        public int compareTo(Object o) {
+            return this.name.compareTo(((Student) o).name);
+        }
+    }
+
+    ArrayList<Student> getDefaultStudentsList() {
+        ArrayList<Student> studentList = new ArrayList<>();
+        String[] arrayNames = {"Ivan", "Natalia", "Artem", "Volodymyr", "Yaroslav", "Mark",
+                "Vasyl", "Danylo", "Max", "Mikhailo", "Khristina", "Oksana", "Olga", "Kira",
+                "Anastasia", "Oleksandra", "Daria", "Tetyana", "Yana", "Nikol", "Yaryna",
+                "Sophia", "Elia", "Nika", "Olena", "Solomia", "Evelina", "Nestor", "Yaromyr"};
+        Random rand = new Random();
+        for (int i = 0; i < 1000; ++i) {
+            studentList.add(new Student(arrayNames[rand.nextInt(arrayNames.length - 1)], rand.nextInt(12)));
+        }
+        return studentList;
+    }
+
+
     @Override
     public void variantTask(ArrayList<Integer> arrayList) {
-
-
-
+        ArrayList<Student> studentList = getDefaultStudentsList();
         try {
             FileWriter file = new FileWriter("Task.txt");
             //Printing start array
-            file.append("Start array\n[");
-            for (int i = 0; i < arrayList.size(); ++i) {
-                file.append(" " + arrayList.get(i));
-            }
-            file.append("]");
+            file.append("Starting list\n");
+            studentList.stream().forEach(
+                    student -> {
+                        try {
+                            file.append(student.toString());
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+            );
+            file.append("\n\n");
 
             //MakingTask
+            studentList = (ArrayList<Student>) studentList.stream().filter(student -> student.averageMark > 4).collect(Collectors.toList());
 
-            for (int i = 0; i < arrayList.size(); ++i) {
-                if(arrayList.get(i)<0) arrayList.set(i,(int) Math.sin(arrayList.get(i)));
-            }
+            mergeSort(studentList, studentList.size());
 
             //Printing array
-            file.append("\n\n\nTask completed\n[");
-            for (int i = 0; i < arrayList.size(); ++i) {
-                file.append(" " + arrayList.get(i));
-            }
-            file.append("]");
+
+            file.append("Task DONE list\n");
+            studentList.stream().forEach(
+                    student -> {
+                        try {
+                            file.append(student.toString());
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+            );
+            file.append("\n\n");
             file.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
+
     @Override
     public void sorting(ArrayList<Integer> list, SortingClass.SortingFacilities method, SortingClass.PrintFile printNecessary) throws IOException {
-        SortingClass.mergeSort(list,list.size());
+        SortingClass.mergeSort(list, list.size());
     }
 
     @Override
@@ -50,44 +101,22 @@ public class Task8 implements Task{
             FileWriter file = new FileWriter("Experiment1000.txt");
 
             Random random = new Random();
-            int[][] array = new int[1000][];
-            for (int i = 0; i < array.length; ++i) {
-                array[i] = random.ints(50, 10, 100000).toArray();
+            ArrayList<Integer> arrList = new ArrayList<>();
+            for (int i = 0; i < 1000; ++i) {
+                arrList.add(random.nextInt(10000));
             }
+
             long timeStart = System.nanoTime();
 
-            for(int k=0;k<array.length;++k) {
-                int N = array[k].length;
-                boolean ifSwapWas = false;
-                for (int h = N / 2; h >= 1; h /= 2) {
-                    for (int i = 0; i < N; ++i) {
-                        if (N > i + h) {
-                            if (array[k][i] > array[k][i + h]) {
-                                int temp = array[k][i];
-                                array[k][i] =  array[k][i+h];
-                                array[k][i+h] = temp;
-                                ifSwapWas = true;
-                            }
-                        }
-
-                        if (ifSwapWas) {
-                            for (int j = i; j >= 0; --j) {
-                                if (j - h < 0) break;
-                                else {
-                                    if (array[k][j] < array[k][j - h]) {
-                                        int temp = array[k][j];
-                                        array[k][j] = array[k][j - h];
-                                        array[k][j - h] = temp;
-                                    }
-                                }
-                            }
-                            ifSwapWas = false;
-                        }
-                    }
-
-                }
-                file.append(Arrays.toString(array[k])+"\n");
+            for (int i = 0; i < arrList.size(); ++i) {
+                SortingClass.mergeSort(arrList, arrList.size());
             }
+
+            for (int i = 0; i < 1000; ++i) {
+                file.append(arrList.get(i).toString()).append("\t");
+                if (i % 50 == 0 && i != 0) file.append("\n");
+            }
+
 
             long timeSpent = System.nanoTime() - timeStart;
             file.append("\n\n\n\n" + timeSpent / (1e+9) + "\n\n\n\n");
@@ -98,5 +127,51 @@ public class Task8 implements Task{
 
 
     }
+
+    public static void mergeSort(ArrayList<Student> array, int n) {
+
+        if (n < 2) return;
+        int middleIndex = n / 2;
+
+        ArrayList<Student> leftArray = new ArrayList<Student>();
+        ArrayList<Student> rightArray = new ArrayList<Student>();
+
+
+        for (int i = 0; i < middleIndex; i++) {
+            leftArray.add(array.get(i));
+        }
+        for (int i = middleIndex; i < n; i++) {
+            rightArray.add(array.get(i));
+        }
+
+        mergeSort(leftArray, middleIndex);
+        mergeSort(rightArray, n - middleIndex);
+
+        merge(leftArray, rightArray, array, middleIndex, n - middleIndex);
+
+    }
+
+    private static void merge(ArrayList<Student> leftArray, ArrayList<Student> rightArray, ArrayList<Student> array, int leftSize, int rightSize) {
+
+        int i = 0, j = 0, k = 0;
+
+        while (i < leftSize && j < rightSize) {
+            /*<=*/
+
+            if (leftArray.get(i).compareTo(rightArray.get(j)) < 0) {
+                array.set(k++, leftArray.get(i++));
+            } else {
+                array.set(k++, rightArray.get(j++));
+            }
+        }
+        while (i < leftSize) {
+            array.set(k++, leftArray.get(i++));
+        }
+        while (j < rightSize) {
+            array.set(k++, rightArray.get(j++));
+        }
+
+    }
+
 }
 
